5cb1e8c01f7756e75518df3f4b1d4758987dbaff
alexandr <neglavmag@mail.ru>
2021-12-26 12:29:50 +0300

    check online registration page navigation tests added

---
+++ tests/checkOnlineRegistration/navigation.test.ts
@@ -0,0 +1,64 @@
+import {Browser, BrowserContext, chromium, Page} from "playwright";
+
+describe('Navigation tests for check online registration page', () => {
+    let browser: Browser;
+    let context: BrowserContext;
+    let page: Page;
+
+    let email = "autotest+" + new Date().getTime() + "@dasredatest.ru";
+    let password = "Qwerty123";
+    let username = "Автотест";
+    let faqTitle = "Вопрос-ответ";
+    let landingTitle = "бизнес легко и быстро";
+    let dsTitle = "Деловая среда";
+
+    const url = "https://rbo.uat.dasreda.ru";
+
+    beforeAll(async () => {
+        browser = await chromium.launch({
+            headless: false
+        });
+        context = await browser.newContext()
+        page = await context.newPage();
+
+        await page.goto(url);
+        await page.click("text='Войти'");
+        await page.click("text='Зарегистрироваться'");
+
+        await page.fill("#lastName", username);
+        await page.fill("#firstName", username);
+        await page.fill("#middleName", username);
+        await page.fill("#email", email);
+        await page.fill("//div[contains(@class, 'Registration__registration-form')]//input[@id='password']", password);
+        await page.fill("#passwordMatch", password);
+        await page.click("#personalDataAgreement");
+        await page.click("#test-regForm-singup_button");
+
+        await page.reload();
+        await page.click("text='Войти'");
+        await page.fill("#username", email);
+        await page.fill("#password", password);
+        await page.click("#test-loginForm-singIn");
+        await page.waitForSelector("#test-landing-navPanel-logedIn");
+    });
+
+    test ('Clicking on logo in ip/3 page', async () => {
+        await page.goto(url + "/rbidos/personal-information/ip/3");
+        await page.click("//div[contains(@class, 'topmenu-logo-pic')]");
+        const title = await page.$("#test-landing-header-text");
+        expect(await title.textContent()).toContain(landingTitle);
+    });
+
+    test ('Clicking on logo in ooo/3 page', async () => {
+        await page.goto(url + "/rbidos/personal-information/ooo/3");
+        await page.click("//div[contains(@class, 'topmenu-logo-pic')]");
+        const title = await page.$("#test-landing-header-text");
+        expect(await title.textContent()).toContain(landingTitle);
+    });
+
+    afterAll(async () => {
+        await page.close();
+        await context.close();
+        await browser.close();
+    });
+});
