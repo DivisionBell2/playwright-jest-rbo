79e4068abf28091ebb913b0401a122646a9bd3e4
alexandr <neglavmag@mail.ru>
2022-01-04 15:26:08 +0300

    added faqPage/navigation.test.ts

---
+++ tests/faqPage/navigation.test.ts
@@ -0,0 +1,31 @@
+import {Browser, BrowserContext, chromium, Page} from "playwright";
+
+describe('Navigation tests for FAQ page', () => {
+    let browser: Browser;
+    let context: BrowserContext;
+    let page: Page;
+
+    const landingTitle = "бизнес легко и быстро";
+
+    beforeAll(async () => {
+        browser = await chromium.launch({
+            headless: false
+        });
+        context = await browser.newContext();
+        page = await context.newPage();
+        await page.goto("https://rbo.uat.dasreda.ru/rbidos/faq");
+    });
+
+    test ('Clicking on logo', async () => {
+        await page.click("//div[contains(@class, 'topmenu-logo-pic')]");
+        await page.waitForNavigation();
+        const title = await page.$("#test-landing-header-text");
+        expect(await title.textContent()).toContain(landingTitle);
+    });
+
+    afterAll(async () => {
+        await page.close();
+        await context.close();
+        await browser.close();
+    });
+});
