c05e10f9a6b45cda489f474418948d2b8361525a
alexandr <neglavmag@mail.ru>
2022-01-07 13:04:36 +0300

    new test in tests/feedbackPage/navigation.test.ts

--- tests/feedbackPage/navigation.test.ts
+++ tests/feedbackPage/navigation.test.ts
@@ -16,11 +16,17 @@ describe('Navigation tests for feedback page', () => {
         await page.goto("https://rbo.uat.dasreda.ru/rbidos/feedback");
     });
 
-    test ('Clicking on logo', async () => {
+    test('Clicking on logo', async () => {
         await page.click("//div[contains(@class, 'topmenu-logo-pic')]");
         await page.waitForNavigation();
         const title = await page.$("#test-landing-header-text");
         expect(await title.textContent()).toContain(landingTitle);
+    });
+
+    test('Open youtube frame from header', async () => {
+        await page.click("//span[@role='button' and contains(., 'Видеоинструкция')]");
+        const frame = page.frame("//iframe[@title='YouTube video player']");
+        await page.click("//i[@aria-label='icon: close']");
     });
 
     afterAll(async () => {
