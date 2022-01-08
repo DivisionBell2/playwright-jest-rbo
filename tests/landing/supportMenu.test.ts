53d5db2eda06141cb2facfdd0621184cc726a831
alexandr <neglavmag@mail.ru>
2022-01-08 20:23:19 +0300

    new test in tests/landing/supportMenu.test.ts

--- tests/landing/supportMenu.test.ts
+++ tests/landing/supportMenu.test.ts
@@ -21,9 +21,28 @@ describe("Working of support menu", () => {
         await page.isHidden("//div[@role='button' and contains(@class, 'SupportCall__messenger-opened')]");
     });
 
+    test("Open and close support phone popup", async () => {
+        await page.click("#test-cookieAlert_button");
+        await page.click("//div[@role='button' and contains(@class, 'SupportCall__messenger')]");
+        await page.click("//div[@role='button' and contains(@class, 'SupportCall__messenger-phone')]");
+        const supportPhonePopup = await page.$("//div[@role='dialog']");
+        expect(await supportPhonePopup.isVisible()).toBe(true);
+        await page.click("//button[@aria-label='Close']");
+
+        // Переписать в отдельную функцию с двумя аргументами селектор и условие
+        for (let i = 0; i < 3; i++) {
+            const displayStyleSupportPhonePopup = await page.$("//div[@role='dialog']");
+            if (await displayStyleSupportPhonePopup.getAttribute("style") == null) {
+                await page.waitForTimeout(1000);
+                continue;
+            }
+            expect(await displayStyleSupportPhonePopup.getAttribute("style")).toEqual("display: none;");
+        }
+    });
+
     afterEach(async () => {
         await page.close();
         await context.close();
         await browser.close();
     });
-})
+});
